ad5025aa46da81443147f2b528b842a2d63264fc
Maxim Dubrovsky <maxdubmors@gmail.com>
2026-04-10 11:21:23 +0500

    fix edge to edge

--- feature/levels/impl/src/main/kotlin/dev/stekl0/nonokt/feature/levels/impl/LevelsScreen.kt
+++ feature/levels/impl/src/main/kotlin/dev/stekl0/nonokt/feature/levels/impl/LevelsScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.PrimaryTabRow
@@ -106,7 +107,7 @@ private fun LevelsTabRow(
 ) {
     PrimaryTabRow(
         selectedTabIndex = selectedTab.ordinal,
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier.fillMaxWidth().statusBarsPadding(),
     ) {
         Tab.entries.forEachIndexed { index, tab ->
             Tab(
